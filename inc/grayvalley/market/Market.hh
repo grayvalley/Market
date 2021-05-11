84d4b11a0f15a629c70a828f801b54b8bf5bf0bd
Juha Hellen <juha.hellen@aalto.fi>
2021-05-11 18:04:52 +0300

    add: jason parsing

--- inc/grayvalley/market/Market.hh
+++ inc/grayvalley/market/Market.hh
@@ -9,160 +9,125 @@
 #include <nlohmann/json.hpp>
 
 #include <grayvalley/common/Macros.hh>
-#include <grayvalley/lob/OrderBook.hh>
+#include <grayvalley/lob/MBOOrderBook.hh>
 
 
 namespace GVT {
-    class Instrument {
-    public:
-        std::string Name;
-        std::string Exchange;
-        uint64_t Id;
-        uint64_t PriceDecimals;
-    public:
 
-        Instrument() = default;
-
-        Instrument(std::string name, std::string exchange, uint64_t id, uint64_t decimals):
-            Name{name}, Exchange{exchange}, Id{id}, PriceDecimals{decimals} {}
-
-        Instrument(const Instrument& other) {
-            Name = other.Name;
-            Exchange = other.Exchange;
-            Id = other.Id;
-            PriceDecimals = other.PriceDecimals;
-        }
-
-    public:
-        [[nodiscard]] std::string description() const;
+    struct Instrument {
+        std::string symbol;
+        uint64_t decimals;
     };
-}
 
-namespace GVT {
-    std::string Instrument::description() const {
-        std::stringstream ss;
-        ss << "Instrument ";
-        ss << "Name: " << Name << ", ";
-        ss << "Id: " << Id << std::endl;
-        return ss.str();
+    void from_json(const nlohmann::json& j, Instrument& inst) {
+        j.at("symbol").get_to(inst.symbol);
+        j.at("price-decimals").get_to(inst.decimals);
     }
 }
 
-namespace GVT::Instruments {
 
-    using InstrumentId = std::variant<std::string, uint64_t>;
-
-    template<typename... T>
-    constexpr auto makeName(T&&... args){
-        return InstrumentId(std::in_place_index<0>, std::forward<T>(args)...);
-    }
-
-    template<typename... T>
-    constexpr auto makeId(T&&... args){
-        return InstrumentId(std::in_place_index<1>, std::forward<T>(args)...);
-    }
-}
 
 namespace GVT {
     class InstrumentStore {
-        using KeyType = GVT::Instruments::InstrumentId;
-        using ValueType = GVT::Instrument;
-        using StoreType = std::unordered_map<KeyType, ValueType>;
+        using StoreType = std::unordered_map<std::string, GVT::Instrument>;
     protected:
         StoreType mItems;
     public:
-
         InstrumentStore() = default;
-
         InstrumentStore(const InstrumentStore& other) {
             for (const auto & item : other.mItems) {
                 mItems.insert(item);
             }
         }
-
     public:
-
-        void put(KeyType k, ValueType v) {
-            mItems.insert(std::make_pair(k, std::move(v)));
+        void put(const std::string& key, GVT::Instrument value) {
+            mItems.insert(std::make_pair(key, std::move(value)));
         }
-
-        std::optional<GVT::Instrument> get(const KeyType& k) const {
-
-            auto it = mItems.find(k);
-
+        void put(GVT::Instrument instrument) {
+            mItems.insert(std::make_pair(instrument.symbol, std::move(instrument)));
+        }
+        std::optional<GVT::Instrument> get(const std::string& key) const {
+            auto it = mItems.find(key);
             if (it == mItems.end()){
                 return std::nullopt;
             }
-
             return it->second;
         }
-
         StoreType& items() {
             return mItems;
         }
-
         const StoreType& items() const {
             return mItems;
         }
-
         StoreType::const_iterator begin() const {
             return mItems.begin();
         }
-
         StoreType::const_iterator end() const {
             return mItems.end();
         }
-
     };
+
+    void from_json(const nlohmann::json& j, InstrumentStore& store) {
+       for (auto & item : j){
+           auto instrument = item.get<GVT::Instrument>();
+           store.put(instrument);
+       }
+    }
 }
 
 namespace GVT {
+
+    struct Exchange {
+        std::string name;
+        GVT::InstrumentStore instruments;
+        GVT::Session market_data;
+        GVT::Session order_entry;
+    };
+
+    void from_json(const nlohmann::json& j, Exchange& exchange) {
+        j.at("name").get_to(exchange.name);
+        j.at("instruments").get_to(exchange.instruments);
+        j.at("market-data").get_to(exchange.market_data);
+        j.at("order-entry").get_to(exchange.order_entry);
+    }
+}
+
+namespace GVT {
+    template<class OrderBookType = GVT::LOB::MBOOrderBook>
     class Market {
     public:
-        GVT::Instrument Instrument;
-        GVT::LOB::OrderBook OrderBook;
+        GVT::Instrument instrument;
+        OrderBookType OrderBook;
     public:
-        explicit Market(GVT::Instrument instrument) : Instrument(instrument) {};
+        explicit Market(GVT::Instrument instrument) : instrument(std::move(instrument)) {};
     };
 }
 
 namespace GVT {
+    template<class OrderBookType = GVT::LOB::MBOOrderBook>
     class MarketStore {
-        using KeyType = GVT::Instruments::InstrumentId;
-        using ValueType = std::unique_ptr<GVT::Market>;
-        using StoreType = std::unordered_map<KeyType, ValueType>;
+        using ValueType = std::unique_ptr<GVT::Market<OrderBookType> >;
+        using StoreType = std::unordered_map<std::string, ValueType>;
     private:
-        StoreType mItems;
+        StoreType m_store;
     public:
-        const std::string Name;
-        const uint64_t Id;
-    public:
-        explicit MarketStore(std::string name, uint64_t id): Name(std::move(name)), Id(id) {};
+        explicit MarketStore() = default;
     public:
 
-        void populate(const GVT::InstrumentStore& store) {
-            for (auto & item : store) {
-                if (item.second.Exchange == Name) {
-                    auto ptr = std::make_unique<GVT::Market>(item.second);
-                    mItems.emplace(item.first, std::move(ptr));
-                }
+        void populate(const GVT::InstrumentStore& instruments) {
+            for (auto & item : instruments) {
+                auto ptr = std::make_unique<GVT::Market<OrderBookType>>(item.second);
+                m_store.emplace(item.first, std::move(ptr));
+
             }
         }
 
-        std::optional<Market*> get(const KeyType& k) {
-            auto it = mItems.find(k);
-            if (it == mItems.end()){
+        std::optional<Market<OrderBookType>*> get(const std::string& k) {
+            auto it = m_store.find(k);
+            if (it == m_store.end()){
                 return std::nullopt;
             }
             return it->second.get();
-        }
-
-        StoreType::const_iterator begin() const {
-            return mItems.begin();
-        }
-
-        StoreType::const_iterator end() const {
-            return mItems.end();
         }
     };
 }
